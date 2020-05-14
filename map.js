71e67b9cbae09ed0848ba3b7bc89035afe5b5d4e
BuildTools <unconfigured@null.spigotmc.org>
2020-05-14 22:00:24 +0200

    Repo init

---
+++ map.js
@@ -0,0 +1,23 @@
+const p0_crs = L.extend({}, L.CRS.Simple, {
+    transformation: new L.Transformation(1/32, 0, 1/32, 0),
+});
+
+const bounds = [[362,0], [7832,8192]];
+const map = L.map('map', {
+    crs: p0_crs,
+    maxBounds: bounds
+}).fitBounds(bounds);
+
+L.tileLayer('images/{z}/{x}/{y}.jpg', {
+    minZoom: 2,
+    maxZoom: 5,
+    noWrap: true,
+    bounds: bounds,
+    useCache: true,
+    crossOrigin: true
+}).addTo(map);
+
+
+map.on('click', e => {
+   console.log(e.latlng)
+});

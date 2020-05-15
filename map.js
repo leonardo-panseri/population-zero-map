bd8ece47fec39b57091ac6ca62c29f907cb34ab8
leonardo-panseri <leonardo.panseri@gmail.com>
2020-05-15 19:03:27 +0200

    Added comments and a popup displaying quadrant name to map.js

--- map.js
+++ map.js
@@ -1,7 +1,13 @@
+/* A coordinate reference system that maps 1 pixel to 1 map unit
+   The tiles are 256x256px, so Leaflet CRS.Simple uses by default latitude and longitude 0 to 256
+   The tiles cover an 8192x8192px area, so the scale factor is 1/32
+   The coordinates start from the top left corner and are represented in the code as [y,x]
+*/
 const p0_crs = L.extend({}, L.CRS.Simple, {
     transformation: new L.Transformation(1/32, 0, 1/32, 0),
 });
 
+// Bounds of the map, cutting out unnecessary tiles
 const bounds = [[362,0], [7832,8192]];
 const map = L.map('map', {
     crs: p0_crs,
@@ -17,7 +23,22 @@ L.tileLayer('images/map/{z}/{x}/{y}.jpg', {
     crossOrigin: true
 }).addTo(map);
 
-
+// On click open a popup displaying the quadrant name
 map.on('click', e => {
-   console.log(e.latlng)
+    L.popup()
+        .setLatLng(e.latlng)
+        .setContent('<p>Quadrant: ' + getQuadrant(e.latlng) + '</p>')
+        .openOn(map);
 });
+
+// Transform map coordinates in quadrant name
+function getQuadrant(latlng) {
+    // The quadrants are 370x370px and the first quadrant starts at [395,16]
+    let y = Math.floor((latlng.lat-395)/370);
+    let x = Math.floor((latlng.lng-16)/370) + 1;
+
+    // Get the ASCII code of the capital letter representing the quadrant
+    y = Math.abs(y-19) + 65;
+
+    return String.fromCharCode(y) + " " + x;
+}
